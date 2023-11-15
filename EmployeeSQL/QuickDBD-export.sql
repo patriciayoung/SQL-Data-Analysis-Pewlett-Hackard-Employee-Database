0526a30a6e5e92bafe50b85a9262350fb24a6437
Patricia Young <patriciayoung@Patricias-MacBook-Air.local>
2023-11-15 16:55:56 -0500

    data modeling png file, read me file update, and sql code from pgadmin

---
+++ EmployeeSQL/QuickDBD-export.sql
@@ -0,0 +1,69 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "Departments" (
+    "dept_no" varchar(5)   NOT NULL,
+    "dept_name" varchar(30)   NOT NULL,
+    CONSTRAINT "pk_Departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "Dept_emp" (
+    "emp_no" INT   NOT NULL,
+    "dept_no" varchar(5)   NOT NULL
+);
+
+CREATE TABLE "Dept_manager" (
+    "dept_no" varchar(5)   NOT NULL,
+    "emp_no" INT   NOT NULL
+);
+
+CREATE TABLE "Employees" (
+    "emp_no" INT   NOT NULL,
+    "emp_title_id" varchar(5)   NOT NULL,
+    "birth_date" DATE   NOT NULL,
+    "first_name" varchar(30)   NOT NULL,
+    "last_name" varchar(30)   NOT NULL,
+    "sex" char(1)   NOT NULL,
+    "hire_date" DATE   NOT NULL,
+    CONSTRAINT "pk_Employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "Salaries" (
+    "emp_no" INT   NOT NULL,
+    "salary" INT   NOT NULL,
+    CONSTRAINT "pk_Salaries" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "Titles" (
+    "title_id" varchar(5)   NOT NULL,
+    "title" varchar(20)   NOT NULL,
+    CONSTRAINT "pk_Titles" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+ALTER TABLE "Departments" ADD CONSTRAINT "fk_Departments_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Dept_emp" ("dept_no");
+
+ALTER TABLE "Dept_manager" ADD CONSTRAINT "fk_Dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Departments" ("dept_no");
+
+ALTER TABLE "Dept_manager" ADD CONSTRAINT "fk_Dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+ALTER TABLE "Employees" ADD CONSTRAINT "fk_Employees_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Dept_emp" ("emp_no");
+
+ALTER TABLE "Employees" ADD CONSTRAINT "fk_Employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "Titles" ("title_id");
+
+ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # sql-challenge
+
+EmployeeSQL contains 2 SQL files (employee_tables.sql) and ( ): 
+	1. employee_tables.sql - file builds builds and creates the tables that hold employees 		related data from imported civ files. Relationships are defined between tables and columns 	using primary keys, foreign keys, and composite keys.
+	
+	2. 
+
+Background: conduct research on employees working in 80's and 90's at company. Only data remaining are 6 CSV files
+
+Objs: project consists of designing tables that will hold CSV data, import CSVs into a SQL database, and answer questions about data.
+
+1. Data Modeling
+2. Data Engineering
+3. Data Analysis
+
+Data Modeling: after inspecting CSV files, create ERC using Quick Database Diagrams.
+
+Data Engineering: Create table schema for each of the CSV files with specific data types, primary keys, foreign keys, and constraints. (Hint: import data in same order as corresponding tables got created. Remember to account for the headers when doing the imports.)
+
+Data Analysis: 
+
+List the employee number, last name, first name, sex, and salary of each employee.
+
+List the first name, last name, and hire date for the employees who were hired in 1986.
+
+List the manager of each department along with their department number, department name, employee number, last name, and first name.
+
+List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
+
+List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
+
+List each employee in the Sales department, including their employee number, last name, and first name.
+
+List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
